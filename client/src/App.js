7ea5f28237bbe2b73f999d44186456710c992a5b
Manuela <manue93_55@hotmail.com>
2021-08-20 13:22:15 -0500

    create Post

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header/Header.js';
+import Post from './components/Post/Post.js'
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Post/>
     </div>
   );
 }
